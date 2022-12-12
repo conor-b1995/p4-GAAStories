104b2141cddfa27284911d9ddbef699d1124fdeb
conor-b1995 <conor_carricktown@hotmail.com>
2022-12-12 14:17:07 +0000

    Add the view for Post List and get started on the base and index.html templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
+
 
 # Create your views here.
+class PostList(ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_date')
+    template_name = 'index.html'
+    paginate_by = 6
